da0aa20e0a932417e99748323d8e67038318a6ac
Terry <t3rrygong@gmail.com>
2020-05-18 18:51:50 -0600

    connectionjs

--- assets/js/tables.js
+++ assets/js/tables.js
@@ -1,13 +1,5 @@
-const mysql = require("mysql");
 const cTable = require("console.table");
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "employee_info_db"
-});
+const connection = require("./connection")
 
 class Tables {
     constructor() {
